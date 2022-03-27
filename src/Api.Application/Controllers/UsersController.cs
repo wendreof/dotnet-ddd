60ced997ecab08e3cc2e776455625f6db416b505
Wendreo Fernandes <wendreo@outlook.com>
2022-03-27 14:27:15 -0300

    Modified: UsersController

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -1,9 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Application.Controllers
 {
+  [Route("api/[controller]")]
+  [ApiController]
   public class UsersController : ControllerBase
   {
+    [HttpGet]
+    public async Task<ActionResult> GetAll([FromServices] IUserService service)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      try
+      {
+        return Ok(await service.GetAll());
+      }
+      catch (ArgumentException ex)
+      {
+        return StatusCode(500, ex.Message);
+      }
+    }
 
   }
 }
