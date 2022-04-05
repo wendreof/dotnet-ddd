29c06cb8721b05c8a10ac26d6996b341822638a3
Wendreo Fernandes <wendreo@outlook.com>
2022-04-04 22:21:35 -0300

    Added: LoginDto

---
+++ src/Api.Domain/Dtos/LoginDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Dtos
+{
+  public class LoginDto
+  {
+    [Required(ErrorMessage = "E-mail eh obrigatorio")]
+    [EmailAddress(ErrorMessage = "E-mail invalido")]
+    [StringLength(100, ErrorMessage = "E-mail deve ter no maximo 100 caracteres")]
+    public string Email { get; set; }
+  }
+}
