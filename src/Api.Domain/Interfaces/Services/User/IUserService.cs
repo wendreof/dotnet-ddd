1f7dc63a112d47a9be04c057d5980c2bff993af5
Wendreo Fernandes <wendreo@outlook.com>
2022-04-17 00:12:18 -0300

    Refactoring: UserService and IUserService

--- src/Api.Domain/Interfaces/Services/User/IUserService.cs
+++ src/Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -1,16 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Api.Domain.Dtos.User;
 using Api.Domain.Entities;
 
 namespace Api.Domain.Interfaces.Services.User
 {
   public interface IUserService
   {
-    Task<UserEntity> Get(Guid id);
-    Task<IEnumerable<UserEntity>> GetAll();
-    Task<UserEntity> Post(UserEntity userEntity);
-    Task<UserEntity> Put(UserEntity userEntity);
+    Task<UserDto> Get(Guid id);
+    Task<IEnumerable<UserDto>> GetAll();
+    Task<UserDtoCreateResult> Post(UserDto userEntity);
+    Task<UserDtoUpdateResult> Put(UserDto userEntity);
     Task<bool> Delete(Guid id);
   }
 }
