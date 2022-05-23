01d733097f35fe9aaaa297b552317f1f6aceb717
Wendreo Fernandes <wendreo@outlook.com>
2022-05-23 20:44:01 -0300

    Modified: Using Faker.NetCore

--- src/Api.Data.Test/UserCompleteCrud.cs
+++ src/Api.Data.Test/UserCompleteCrud.cs
@@ -10,31 +10,31 @@ namespace Api.Data.Test
 {
   public class UserCompleteCrud : BaseTest, IClassFixture<DbTeste>
   {
-    private ServiceProvider _serviceProvider { get; set; }
+    private ServiceProvider ServiceProvider { get; set; }
 
     public UserCompleteCrud(DbTeste dbTeste)
     {
-      _serviceProvider = dbTeste.ServiceProvider;
+      ServiceProvider = dbTeste.ServiceProvider;
     }
 
     [Fact(DisplayName = "User Crud")]
     [Trait("CRUD", "UserEntity")]
     public async Task Is_Possible_Realize_User_Crud()
     {
-      using var context = _serviceProvider.GetService<MyContext>();
+      using var context = ServiceProvider.GetService<MyContext>();
 
       UserImplementation _repository = new UserImplementation(context);
 
       UserEntity _entity = new UserEntity
       {
-        Email = "teste@mail.com",
-        Name = "Teste"
+        Email = Faker.Internet.Email(),
+        Name = Faker.Name.FullName(),
       };
 
       var _createdRegistry = await _repository.InsertAsync(_entity);
       Assert.NotNull(_createdRegistry);
-      Assert.Equal("teste@mail.com", _createdRegistry.Email);
-      Assert.Equal("Teste", _createdRegistry.Name);
+      Assert.Equal(_entity.Email, _createdRegistry.Email);
+      Assert.Equal(_entity.Name, _createdRegistry.Name);
       Assert.False(_createdRegistry.Id == Guid.Empty);
     }
   }
