6943f02b5156318dfd3187008f30d1fbc4b1cad9
Wendreo Fernandes <wendreo@outlook.com>
2022-05-28 14:47:26 -0300

    Added: BaseTestService

---
+++ src/Api.Service.Test/BaseTestService.cs
@@ -0,0 +1,36 @@
+using System;
+using Api.CrossCutting.Mappings;
+using AutoMapper;
+
+namespace Api.Service.Test
+{
+  public abstract class BaseTestService
+  {
+    public IMapper Mapper { get; set; }
+
+    public BaseTestService()
+    {
+      Mapper = new AutoMapperFixture().GetMapper();
+    }
+
+    public class AutoMapperFixture : IDisposable
+    {
+      public IMapper GetMapper()
+      {
+        var config = new MapperConfiguration(cfg =>
+        {
+          cfg.AddProfile(new ModelToEntityProfile());
+          cfg.AddProfile(new DtoToModelProfile());
+          cfg.AddProfile(new EntityToDtoProfile());
+        });
+
+        return config.CreateMapper();
+      }
+
+      public void Dispose()
+      {
+      }
+    }
+  }
+}
+
