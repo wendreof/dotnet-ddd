a963f1426f1ada0e41013cbb69113a8059a3ecd3
Wendreo Fernandes <wendreo@outlook.com>
2022-05-29 13:07:40 -0300

    Test DotnetFormatter

--- src/Api.Application/Controllers/LoginController.cs
+++ src/Api.Application/Controllers/LoginController.cs
@@ -9,42 +9,42 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Application.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class LoginController : ControllerBase
-  {
-
-    [AllowAnonymous]
-    [HttpPost]
-    public async Task<object> Login([FromBody] LoginDto loginDto, [FromServices] ILoginService loginService)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
     {
-      if (!ModelState.IsValid)
-      {
-        return BadRequest(ModelState);
-      }
 
-      if (loginDto == null)
-      {
-        return BadRequest();
-      }
-
-      try
-      {
-        var result = await loginService.FindByLogin(loginDto);
-        if (result != null)
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<object> Login([FromBody] LoginDto loginDto, [FromServices] ILoginService loginService)
         {
-          return Ok(result);
-        }
-        else
-        {
-          return NotFound();
-        }
-      }
-      catch (ArgumentException ex)
-      {
-        return StatusCode(500, ex.Message);
-      }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            if (loginDto == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var result = await loginService.FindByLogin(loginDto);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
     }
-  }
 }
--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -8,128 +8,128 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Application.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class UsersController : ControllerBase
-  {
-    private readonly IUserService _service;
-    public UsersController(IUserService service)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
     {
-      _service = service;
-    }
-
-    [Authorize("Bearer")]
-    [HttpGet]
-    public async Task<ActionResult> GetAll()
-    {
-      if (!ModelState.IsValid)
-      {
-        return BadRequest(ModelState);
-      }
-
-      try
-      {
-        return Ok(await _service.GetAll());
-      }
-      catch (ArgumentException ex)
-      {
-        return StatusCode(500, ex.Message);
-      }
-    }
-
-    [Authorize("Bearer")]
-    [HttpGet]
-    [Route("{id}", Name = "GetWithId")]
-    public async Task<ActionResult> Get(Guid id)
-    {
-      if (!ModelState.IsValid)
-      {
-        return BadRequest(ModelState);
-      }
-
-      try
-      {
-        return Ok(await _service.Get(id));
-      }
-      catch (ArgumentException ex)
-      {
-        return StatusCode(500, ex.Message);
-      }
-    }
-
-    [Authorize("Bearer")]
-    [HttpPost]
-    public async Task<ActionResult> Post([FromBody] UserDtoCreate user)
-    {
-      if (!ModelState.IsValid)
-      {
-        return BadRequest(ModelState);
-      }
-
-      try
-      {
-        var result = await _service.Post(user);
-        if (result != null)
+        private readonly IUserService _service;
+        public UsersController(IUserService service)
         {
-          return Created(new Uri(Url.Link("GetWithId", new { id = result.Id })), result);
+            _service = service;
         }
-        else
+
+        [Authorize("Bearer")]
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
         {
-          return BadRequest();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.GetAll());
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
-      }
-      catch (ArgumentException ex)
-      {
-        return StatusCode(500, ex.Message);
-      }
-    }
 
-    [Authorize("Bearer")]
-    [HttpPut]
-    public async Task<ActionResult> Put([FromBody] UserDtoUpdate user)
-    {
-      if (!ModelState.IsValid)
-      {
-        return BadRequest(ModelState);
-      }
-
-      try
-      {
-        var result = await _service.Put(user);
-        if (result != null)
+        [Authorize("Bearer")]
+        [HttpGet]
+        [Route("{id}", Name = "GetWithId")]
+        public async Task<ActionResult> Get(Guid id)
         {
-          return Ok(result);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.Get(id));
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
-        else
+
+        [Authorize("Bearer")]
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UserDtoCreate user)
         {
-          return BadRequest();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Post(user);
+                if (result != null)
+                {
+                    return Created(new Uri(Url.Link("GetWithId", new { id = result.Id })), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
-      }
-      catch (ArgumentException ex)
-      {
-        return StatusCode(500, ex.Message);
-      }
+
+        [Authorize("Bearer")]
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] UserDtoUpdate user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Put(user);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [Authorize("Bearer")]
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.Delete(id));
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
     }
-
-    [Authorize("Bearer")]
-    [HttpDelete]
-    [Route("{id}")]
-    public async Task<ActionResult> Delete(Guid id)
-    {
-      if (!ModelState.IsValid)
-      {
-        return BadRequest(ModelState);
-      }
-
-      try
-      {
-        return Ok(await _service.Delete(id));
-      }
-      catch (ArgumentException ex)
-      {
-        return StatusCode(500, ex.Message);
-      }
-    }
-
-  }
 }
