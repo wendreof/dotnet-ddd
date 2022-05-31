419d89627f3ae403795fb16e3c2f4cf5cb8e4a7f
Wendreo Fernandes <wendreo@outlook.com>
2022-05-30 22:28:06 -0300

    Added: ItsPossibleExecuteDeleteMethod

---
+++ src/Api.Service.Test/User/WhenExecuteDelete.cs
@@ -0,0 +1,24 @@
+using System;
+using Api.Domain.Interfaces.Services.User;
+using Moq;
+using Xunit;
+
+namespace Api.Service.Test.User
+{
+  public class WhenExecuteDelete : UserTests
+  {
+    private IUserService _userService;
+    private Mock<IUserService> _userServiceMock;
+
+    [Fact(DisplayName = "It's possibile execute Delete method")]
+    public async void ItsPossibleExecuteDeleteMethod()
+    {
+      _userServiceMock = new Mock<IUserService>();
+      _userServiceMock.Setup(x => x.Delete(It.IsAny<Guid>())).ReturnsAsync(true);
+      _userService = _userServiceMock.Object;
+
+      var _result = await _userService.Delete(UserId);
+      Assert.True(_result);
+    }
+  }
+}
